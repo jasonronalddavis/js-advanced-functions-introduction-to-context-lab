0a1d3e21a3a1eb86febfc775e20915ea984ddb84
jasonronalddavis <jdavis@c.ringling.edu>
2021-08-27 18:59:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function createEmployeeRecord(record){
+    return{
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(recordArray){
+    return recordArray.map(record => createEmployeeRecord(record))
+
+}
+
+function findEmployeeByFirstName(employeeRecords, name){
+    return employeeRecords.find(function(employee){
+        return employee.firstName == name
+    })
+}
+
+
