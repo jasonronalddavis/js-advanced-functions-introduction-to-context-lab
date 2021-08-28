8f7bc9c08ff90a94e688a5f7038c4ebd6072107e
jasonronalddavis <jdavis@c.ringling.edu>
2021-08-27 19:54:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Your code here
+
+
+
+
+
+
 function createEmployeeRecord(record){
     return{
         firstName: record[0],
@@ -20,5 +26,54 @@ function findEmployeeByFirstName(employeeRecords, name){
         return employee.firstName == name
     })
 }
+
+function createTimeInEvent(employeeRecord, timeStamp){
+    let date = timeStamp.split(' ');
+    employeeRecord.timeInEvents.push({type: 'TimeIn', hour: parseInt(date[1]),
+    date: date[0] });
+
+    return employeeRecord
+}
+
+function createTimeOutEvent(employeeRecord, timeStamp){
+    let date = timeStamp.split(' ');
+    employeeRecord.timeOutEvents.push({type: 'TimeOut', hour: parseInt(date[1]),
+    date: date[0] });
+
+    return employeeRecord
+}
+
+function hoursWorkedOnDate(employeeRecord, workRecord){
+    let timeIn = employeeRecord.timeInEvents.find(function(record){
+        return record.date = workRecord
+    })
+    let timeOut = employeeRecord.timeOutEvents.find(function(record){
+        return record.date = workRecord
+    })
+    let hoursWorked =  (timeOut.hour - timeIn.hour) / 100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(employeeRecord, workRecord){
+    let hours = hoursWorkedOnDate(employeeRecord, workRecord)
+    return hours * employeeRecord.payPerHour
+}
+
+function allWagesFor(employeeRecord){
+    let dates = employeeRecord.timeInEvents.map(function(record){
+        return record.date
+    })
+
+    let wages = dates.reduce(function(totalWages, date){
+        return totalWages + wagesEarnedOnDate(employeeRecord, date)
+    }, 0)
+
+    return wages
+}
+function calculatePayroll(employeeRecords){
+    return employeeRecords.reduce(function(totalWorked, employeeRecord){
+        return totalWorked + allWagesFor(employeeRecord)}, 0)
+}
+
 
 
